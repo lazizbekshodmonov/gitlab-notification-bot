f8b749baa5d71af5eb435a2bead8f18c3b59ae7a
Lazizbek Shodmonov <lazizbekshodmonov30@gmail.com>
2025-10-19 23:47:49 +0500

    gitlab telegram bot completed

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import getEnvVariable from './config/env.js';
-import { PrismaClient } from '@prisma/client';
 import webhookRouter from './webhook/webhook.router.js';
 
 const app = express();
@@ -12,12 +11,9 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(webhookRouter);
 
-const prisma = new PrismaClient();
-
 const port = Number(getEnvVariable('PORT'));
 app.listen(port, async (error) => {
   if (error) return console.log(error);
-  prisma.$connect();
   console.log(`🚀 Server running on port ${port}`);
   await bot.start({
     onStart: (botInfo) => {
